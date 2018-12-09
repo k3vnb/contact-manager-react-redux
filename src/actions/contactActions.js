89bcec23eb217382bb58c2f56a7d4aaf05220827
Kevin Boyle <papershack@gmail.com>
2018-12-08 23:51:46 -0800

    integrate json data from jsonplaceholder api

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,19 +1,24 @@
 import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from './types';
+import axios from 'axios'
 
-export const getContacts = () => {
-    return {
-        type: GET_CONTACTS
-    }
-}
+export const getContacts = () => async dispatch => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+
+    dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+    });
+};
 export const deleteContact = (id) => {
     return {
         type: DELETE_CONTACT,
         payload: id
-    }
-}
+    };
+};
 export const addContact = (contact) => {
     return {
         type: ADD_CONTACT,
         payload: contact
-    }
-}
+    };
+};
